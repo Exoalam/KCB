08fcf46e109242da3119f79e9a48d0886deaadbe
Exoalam <nafiul.arpon@gmail.com>
2022-06-30 02:07:59 +0600

    update

--- Data/DataBase.cs
+++ Data/DataBase.cs
@@ -5,23 +5,22 @@ namespace KCB.Data
 {
     public class DataBase
     {
-        public string start_server()
+        public string check_acc(string email, string password)
         {
             MongoClient dbClient = new MongoClient("mongodb+srv://nafiul005:nafiul005@database.tthhr5l.mongodb.net/?retryWrites=true&w=majority");
             var database = dbClient.GetDatabase("Users");
             var collection = database.GetCollection<BsonDocument>("UserLogin");
-            var document = new BsonDocument { { "student_id", 10000 }, {
-                "scores",
-                new BsonArray {
-                new BsonDocument { { "type", "exam" }, { "score", 88.12334193287023 } },
-                new BsonDocument { { "type", "quiz" }, { "score", 74.92381029342834 } },
-                new BsonDocument { { "type", "homework" }, { "score", 89.97929384290324 } },
-                new BsonDocument { { "type", "homework" }, { "score", 82.12931030513218 } }
-                }
-                }, { "class_id", 480 }
-            };
+            var search = new BsonDocument { { "email", email }, { "password", password } };
+            string check = collection.Find(search).FirstOrDefault().ToString();
+            return check;
+        }
+        public void create_acc(string user, string email, string password)
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://nafiul005:nafiul005@database.tthhr5l.mongodb.net/?retryWrites=true&w=majority");
+            var database = dbClient.GetDatabase("Users");
+            var collection = database.GetCollection<BsonDocument>("UserLogin");
+            var document = new BsonDocument { { "user", user }, { "email", email }, { "password", password } };
             collection.InsertOne(document);
-            return "hello";
         }
       
     }
